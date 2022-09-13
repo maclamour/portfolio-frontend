dd1de1eab2393c0583808e3c980c6443a72789a8
maclamour <maclamour@gmail.com>
2022-09-13 15:09:24 -0400

    set up pages and compenets

--- react-portfolio-frontend/src/App.js
+++ react-portfolio-frontend/src/App.js
@@ -1,11 +1,31 @@
-
 import './App.css';
+// IMPORT COMPONENTS
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import { Route, Routes,Routes } from 'react-router';
+// IMPORT PAGES
+import Home from './pages/Home'
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 
 function App() {
+    // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+
+    const URL='https://mackenzie-fullstack-portfolio.herokuapp.com/' 
+  
   return (
     <div className="App">
 
       <h2>Hello</h2>
+    
+    <Header/>
+    <Routes>
+      <Route exact path="/" element={Home}/>
+      <Route path="/projects" element={<Projects URL={URL} />} />
+      <Route path="/about" element={<About URL={URL} />} />
+    </Routes>
+    <Footer/>
       
     </div>
   );
