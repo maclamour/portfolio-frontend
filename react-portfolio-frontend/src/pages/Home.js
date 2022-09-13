ac0e7ec2ef7f9c46b321954225bd01c0935b63d0
maclamour <maclamour@gmail.com>
2022-09-13 16:10:37 -0400

    frontend complete

--- react-portfolio-frontend/src/pages/Home.js
+++ react-portfolio-frontend/src/pages/Home.js
@@ -3,17 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+       
         <h2>Welcome</h2>
         
     {/* <img src= "https://i.imgur.com/6BJEzJal.jpg">
 
     </img> */}
         
-        
-
-
-
-    </div>
+ </div>
 
     
   )
